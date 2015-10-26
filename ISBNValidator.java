2509a8af09e1dd41400aa4725ab8420ad0137822
Adam Collins <adamcollinssecure@gmail.com>
2015-10-25 20:19:02 -0700

    Works with bigger ISBNs and checks if length is correct.

--- ISBNValidator.java
+++ ISBNValidator.java
@@ -1,10 +1,11 @@
 package com.openrants.www;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
-public class ISBNValidator {
+public class Main {
     static Scanner sc;
-    static int isbn;
+    static String isbn;
 
     public static void main(String[] args) {
         sc = new Scanner(System.in);
@@ -15,9 +16,10 @@ public class ISBNValidator {
     private static void getISBN() {
         System.out.print("Please Enter the ISBN: ");
         try {
-            isbn = sc.nextInt();
+            isbn = sc.nextLine();
+            if(isbn.length()!=10) {int x = 5/0;}
         } catch (Exception e) {
-            System.out.println("ISBN is NOT valid.");
+            System.out.println("ISBN is NOT valid..");
             System.exit(0);
         }
         System.out.println("Checking...");
@@ -25,12 +27,14 @@ public class ISBNValidator {
 
     private static void checkISBN() {
         //int number = convertToInt(); //Will allow for books that end in x.
+        long num = Long.parseLong(isbn);
         int total = 0;
+
         for (int i = 0; i <= 10; i++) {
-            int indexValue = (int) (isbn / Math.pow(10, 10 - (i + 1)));
+            int indexValue = (int) (num/ Math.pow(10, 10 - (i + 1)));
             System.out.print(indexValue);
             total += indexValue*(10-i);
-            isbn -= indexValue * Math.pow(10, 10 - (i + 1));
+            num -= indexValue * Math.pow(10, 10 - (i + 1));
         }
         if(total%11==0)
         {
